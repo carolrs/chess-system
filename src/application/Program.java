01e3158d885ce0a746813aeaf4ccedef92c60011
Carolina R <anacarolinarodriguesoares@gmail.com>
2021-05-18 15:10:29 +0100

    Program

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boadgame.Board;
+import boadgame.Position;
+import chess.ChessMatch;
+
+public class Program {
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+}
